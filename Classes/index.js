ca097031fb18dffeb5557e9aac1a86ca1560f918
Ali-Arani <94089011+Ali-Arani@users.noreply.github.com>
2022-09-14 18:31:55 -0400

    week2
    week2

--- Classes/index.js
+++ Classes/index.js
@@ -16,6 +16,6 @@ class Vehicle {
     }
     }
     let car = new Car('blue');
-    car.toString();
+    console.log(car.toString());
     console.log(car instanceof Car);
     console.log(car instanceof Vehicle);
